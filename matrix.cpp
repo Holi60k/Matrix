3a986292a9411afe4511fac110ccd8557b1acdf4
Holozsnyák Nándor <holi60@gmail.com>
2015-06-27 13:11:58 +0200

    comments added - hun

--- matrix.cpp
+++ matrix.cpp
@@ -1,3 +1,7 @@
+//Matrix class made by Holi60k
+//Just for fun
+//Nevermind if it has some bugs, they may be fixed :)
+
 #include <iostream>
 
 template<class T>
@@ -5,7 +9,7 @@ class Matrix {
 
 public:
 
-	
+	//Konstuktor
 	Matrix(int a = 0, int b = 0):siX(a),siY(b)
 	{
 		//inicializálás itt történik, memóriában foglalunk helyet ennek a sok szép számnak...
@@ -23,7 +27,7 @@ public:
 			Squared = false;
 		}
 	};
-
+	//Destruktor
 	~Matrix()
 	{
 		//destruktor törli a sok szép számunkat
@@ -63,7 +67,7 @@ public:
 		return *this;
 			
 	}
-
+	//Másoló konstuktor
 	Matrix (Matrix & t) {
 
 		//std::cout << "copy ctor " << std::endl;
@@ -93,7 +97,7 @@ public:
 	//két mátrix összeadására szolgáló operátorunk
 	Matrix & operator+ (const Matrix & t) {
 
-		std::cout << "Starting add the two Matrixes..." << std::endl;
+		//std::cout << "Starting add the two Matrixes..." << std::endl;
 		if(this->GetY() == t.GetX()) {
 
 			Matrix<T> Result(this->GetX(),t.GetY());
@@ -120,7 +124,7 @@ public:
 	Matrix & operator* (const Matrix & A)
 	{
 		T Var = 0;
-		std::cout << "Starting multiplicate the two Matrixes..." << std::endl;
+		//std::cout << "Starting multiplicate the two Matrixes..." << std::endl;
 		if(this->GetY() == A.GetX())
 		{
 			Matrix<T> Result(this->GetX(),A.GetY());
@@ -157,19 +161,22 @@ public:
 	void FillMatrix(T v, int x, int y) const {
 		Matx[x][y] = v;		
 	}
-
+	//Visszaadja a mátrix sorainak számát
 	int GetX() const {
 		return siX;
 	}
-
+	//Visszadja a mátrix oszlopainak számát
 	int GetY() const {
 		return siY;
 	}
-
+	//visszaadja a mátrix x.sorának y.edik elemét
 	T GetValue(int x, int y) const {
 		return Matx[x][y];
 	}
-
+	//két sor cseréje, elemi sorművelet ugyebár amit majd a Gauss(-Jordan) elimináció során fogunk haszálni
+	//természetesen ezek csak mutató cserék mivel a sorainak egyben mutatók ahova tároljuk az eleminket
+	//ennél a függvénynél található egy Det_Sign *= -1 is amivel már a determinánsunk előjelét is tudjuk váltani,
+	// mivel definíció szerint ha sort vagy oszlopot cserélünk akkor a determináns értéke a -1-szeresére változik.
 	void ChangeRows(int a, int b) {
 		T *cs;
 		cs = Matx[a];
@@ -179,67 +186,62 @@ public:
 		
 		
 	}
-
+	//Visszaadja a mátrix i.edik sorát, igazából nem volt használva de jó hogyha van egy ilyenünk is
 	T* GetRows(int i) { 
 		//std::cout<< i << " - " << Matx[i] << std::endl;
 		return Matx[i];
 	}
+	//visszadja a determinánsunk előjelét
 	int Get_Det_Sign() {
 		return Det_Sign;
 	}
+	//Igaz hogy GaussElimination néven van a függvény de ez lesz nekünk a Gauss-Jordan eliminálónk, mivel felül illetve alul is eliminálunk
+	//Működése elég egyértelmű de rövid mondatokban
+	//1 - a vizsgált sort cseréljük az első sorral(ha az első persze akkor is) és beosztjuk mindig a sorunkat az aktuális i.edik elemmel ez mindig az aktuálisan feldolgozandó sor i.edik eleme
+	//2 - ezután ennek a sornak az X-szeresét (x = a következő sorok i.edik pozíciójában álló eleme) az alatta levő sorokhoz
+	//3 - visszacseréljük a két sort és újraindul az algoritmus
+	//végülis Gauss-Jordan elimináció csak itt nem tűnik fel az alul-felül való elimináció mert mindig sort cserélünk és csak lefelé eliminálunk :)
 	void GaussElimination() {
-		std::cout << "Gauss" << std::endl;
+		std::cout << "Gauss-Jordan" << std::endl;
 
 		for(int i = 0; i < siX; i++) {
 			ChangeRows(0,i);
-			//std::cout << "Multiplicate - " << i << "\nOsztok:" << 1/Matx[0][i]<<std::endl;
-
 			MultiplicateRow(0,Matx[0][i] != 0?1/Matx[0][i]:1);
-			//this->GetWholeMatrix();
-			//std::cout << "-" << std::endl;
-
-			//std::cout << "AddRow2Row - " << i+1 <<std::endl;
 			for(int j = 1; j < siX; j++) {
 
 				AddRow2Row(j,0,(-1)*Matx[j][i]);
-			//	this->GetWholeMatrix();
-			//	std::cout << "--" << std::endl;
 			}
 			ChangeRows(0,i);
-			//std::cout << "---" << std::endl;
-			//this->GetWholeMatrix();
-			//std::cout << "--------------" << std::endl;
-
-			
-			
 		}
 
 	}
-	//ez inkább felső 3-szög alakra hozza a mátrixot :)
+	//Ugyan az mint a rendes GaussElimination() fgv,
+	//de ez inkább felső 3-szög alakra hozza a mátrixot :)
 	void GaussElimination_2() {
 		//std::cout << "Gauss_2" << std::endl;
 		T pivot;
-		for(int i = 0; i < siX; i++) {
-			if(WhereIsItsRow(i) == i){
+		for (int i = 0; i < siX; i++) {
+
+			if (WhereIsItsRow(i) == i){
 				pivot = Matx[i][i] != 0?1/Matx[i][i]:1;
 				MultiplicateRow(i,pivot);
 
-				for(int j = i+1; j < siX; j++) {
+				for (int j = i+1; j < siX; j++) {
 
 					AddRow2Row(j,i,(-1)*Matx[j][i]);
 				}
 
 				MultiplicateRow(i,1/pivot);
-			}
-			else {
+			} else {
 				ChangeRows(i,WhereIsItsRow(i));
 				GaussElimination_2();
 			}
-	}
+		}
 
-		//this->GetWholeMatrix();
 
 	}
+	//Egy sor value-szeresét hozzáadjuk egy másik sor-hoz
+	//a-hoz adjuk hozzá b value szeresét
 	void AddRow2Row(int a, int b, T value) {
 
 		for(int i = 0; i < siY; i++) {
@@ -248,8 +250,11 @@ public:
 		}
 
 	}
+	//Nos igen, az eddigi eliminációk igazából csak homogén rendszerekre volt jó, ennél a függvénynél hozzádobunk egy "vektort" is
+	//De a működés itt is természetesen a Gauss-Jordan...
+	//Szabad paraméterek is működnek elviekben de még nem jól... :(
 	void GaussEliminationWVector(Matrix & b) {
-
+		Matrix<T> Result = *this;
 		std::cout << "Gauss_W_Vector" << std::endl;
 		T pivot;
 		int Free_Param = 0;
@@ -261,7 +266,7 @@ public:
 			MultiplicateRow(0,pivot);
 
 			b.MultiplicateRow(0,pivot);
-			//this->GetWholeMatrixVector(b);
+
 			for(int j = 1; j < siX; j++) {
 				
 				pivot = (-1)* Matx[j][i];
@@ -276,15 +281,16 @@ public:
 	
 			
 		}
-		Free_Param = siY - Rank();
-		std::cout << "Free Parameters: " << Free_Param<< std::endl;
+		Result.GaussElimination_2();
+		Free_Param = siY - Result.Rank();
+		std::cout << "Free Parameters: " << Free_Param << std::endl;
 
 	}
-
-	int Determinant() {
+	//Determináns számítás
+	float Determinant() {
 
 		//std::cout << "Determinant" << std::endl;
-		int Det = 1;
+		float Det = 1;
 		Matrix<T> Result = *this;
 		if(Squared) {
 
@@ -305,8 +311,9 @@ public:
 			return -1;
 		}
 	}
-
+	//Rang számítás... még nem tökéletes a koncepció
 	int Rank() {
+
 		Matrix<T> Result = *this;
 		int rank = 0;
 		Result.GaussElimination();
@@ -314,12 +321,13 @@ public:
 		for(int i = 0; i < siX; i++) {
 
 			if(Result.CountZeroItems(i) == 0) rank++;
-			std::cout << "R: " << rank << std::endl;
+			//std::cout << "R: " << rank << std::endl;
 		}
 
 		return rank;
 
 	}
+	//megszámolja egy sorban lévő 0 elemeket, rangszámításhoz kell...
 	int CountZeroItems(int a) {
 		int Zero = 0;
 
@@ -330,12 +338,10 @@ public:
 		for(int i = 0; i < siY; i++) {
 			if(Matx[a][i] == 0) Zero++;
 		}
-
-		//std::cout<< "Z: " << a << " - " << Zero << std::endl;
-		//std::cout <<"A: " << a << " - " << Matx[a][a] << std::endl;
-		
 		return Zero;
 	}
+	/*
+	//Majdnem jó lett de mégsem...
 	void ReOrderRows() { 
 		int Zero_Num[siX] = {0};
 
@@ -347,7 +353,9 @@ public:
 			}
 		}
 	}
-
+	*/
+	//A következő pár függvény a felső háromszög alakra hozásban játszik szerepet
+	//IsFineRow fügvénnyel meg tudjuk nézni hogy az aktuális sorunk jó helyen van-e, úgymond megfelelő a nullák száma és poziciója
 	bool IsFineRow(int a) {
 
 		int Zero_Num = 0;
@@ -362,7 +370,7 @@ public:
 			return false;
 
 	}
-
+	//Hol kellene hogy az aktuálisan kapott sor legyen, hanyadik sorban
 	int WhereIsItsRow(int a) {
 
 		int Zero_Num = 0;
@@ -375,6 +383,7 @@ public:
 		return Zero_Num;
 
 	}
+	//Rendezzük át a kis mátrixunkat, ez akkor lényeges ha már eleve egy olyan mátrixot kapunk amit egyből felső háromszög alakra tudunk hozni elimináció nélkül
 	void ReOrderTriangle(){
 		// 0 0 1
 		// 1 0 1
@@ -395,7 +404,7 @@ public:
 			
 		}
 	}
-
+	//Már felső háromszög alakú a mátrix?
 	bool IsTriangle() {
 
 		int Zero_Num[siX];
@@ -427,7 +436,7 @@ public:
 			return false;
 
 	}
-
+	//Illetve hogy tudunk-e megfelelő felső háromszög alakra rendezni... 
 	bool CanIReOrderToTriangle() {
 		int Zero_Num[siX];
 		
@@ -436,14 +445,15 @@ public:
 		for(int i = 0; i < siX; i++) {
 		Zero_Num[i] = 0;
 			while(Matx[i][a] == 0 && a < siY) {
-				//std::cout << Matx[i][a] << " ";
+			
 				Zero_Num[i]++;
 				a++;
 			}
-			//std::cout << std::endl;
+			
 			a = 0;
-			//std::cout <<"Folytonos nullák az "<< i << " sorban:" << Zero_Num[i] << std::endl;
+			
 		}
+
 		for(int i = 0; i < siX; i++) {
 		
 			if(Zero_Num[i] > 0){
@@ -457,7 +467,7 @@ public:
 		 return false;
 
 	}
-
+	//a mátrix egy sorának szorzása egy megfelelő számmal
 	void MultiplicateRow(int a, T Multi) {
 
 		for(int i = 0; i < siY; i++) {
@@ -466,7 +476,7 @@ public:
 
 		}
 	}
-
+	//Kiiratja mátrixunkat
 	void GetWholeMatrix() const
 	{
 		std::cout << "---" << std::endl;
@@ -483,6 +493,7 @@ public:
 		std::cout << "---" << std::endl;
 
 	}
+	//Kiiratja a mátrixunkat illetve a paraméterül kapott vektorunkat is 
 	void GetWholeMatrixVector(Matrix & b) const
 	{
 		//std::cout << "Get the whole Martix..." << std::endl;
@@ -498,10 +509,13 @@ public:
 		}
 
 	}
+	//???? lényegtelen még...
 	Matrix Zero_Vector() {
 
 	}
+
 private:
+	//egy pár privát tag.
 	int siX;
 	int siY;
 	bool Squared;
@@ -514,6 +528,7 @@ private:
 
 int main()
 {
+
 	float x = 3,y = 3;
 	//std::cout << "How many rows do you want:";
 	//std:: cin >> x;
@@ -533,24 +548,25 @@ int main()
 			A.FillMatrix(v,i,j);
 		}
 	}*/
-	//X-ek
-	A.FillMatrix(0,0,0);
-	A.FillMatrix(1,0,1);
-	A.FillMatrix(1,0,2);
+	//1. sor
+	A.FillMatrix(-2,0,0);
+	A.FillMatrix(-1,0,1);
+	A.FillMatrix(4,0,2);
 
-	//Y-ok
-	A.FillMatrix(1,1,0);
-	A.FillMatrix(1,1,1);
-	A.FillMatrix(0,1,2);
-	//Z-k
-	A.FillMatrix(0,2,0);
-	A.FillMatrix(0,2,1);
-	A.FillMatrix(1,2,2);
+	//2. sor
+	A.FillMatrix(2,1,0);
+	A.FillMatrix(3,1,1);
+	A.FillMatrix(-1,1,2);
+
+	//3. sor
+	A.FillMatrix(-4,2,0);
+	A.FillMatrix(-10,2,1);
+	A.FillMatrix(-5,2,2);
 	
 	std::cout << "Fill the Vector" << std::endl;
-	W.FillMatrix(2,0,0);
-	W.FillMatrix(-2,1,0);
-	W.FillMatrix(-3,2,0);
+	W.FillMatrix(3,0,0);
+	W.FillMatrix(1,1,0);
+	W.FillMatrix(-12,2,0);
 	/*for(int i = 0; i < x; i++)
 	{
 		for(int j = 0; j<1; j++)
@@ -569,25 +585,15 @@ int main()
 			B.FillMatrix(v,i,j);
 		}
 	}*/
+
 	A.GetWholeMatrix();
-	//B.GetWholeMatrix();
-	//A.ChangeRows(0,1);
-	//A.GetWholeMatrix();
-	//A.GaussEliminationWVector(W);
-    //A.GetWholeMatrixVector(W);
-	//std::cout << "A Determinant:" << A.Determinant() << std::endl;
-	if(A.IsTriangle()) {
-		std::cout << "YEAH it is a triangle formed!" << std::endl;
-		//std::cout << "A1 Determinant:" << A.Determinant() << std::endl;
-	} else {
-		if(A.CanIReOrderToTriangle())
-			//A.ReOrderTriangle();
-		std::cout << "A2 Determinant:" << A.Determinant() << std::endl;
-	}
-	
+	A.GaussEliminationWVector(W);
+    A.GetWholeMatrixVector(W);
+	std::cout << "A Determinant:" << A.Determinant() << std::endl;
+		
 	//A.GetWholeMatrix();
 	//A.GaussElimination_2();
-	A.GetWholeMatrix();
+	//A.GetWholeMatrixVector(W);
 	//std::cout << "A rangja:" << A.Rank() << std::endl;
 	//B.GetWholeMatrix();
 	//C = A;
